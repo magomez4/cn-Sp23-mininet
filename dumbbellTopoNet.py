5f3d5db1718e474b76dc322dc25c72c0ce836c78
None <None>
2023-03-28 00:13:01 -0700

    WIP. calculated link bandwidths. need to add delays and other parameters

--- dumbbellTopoNet.py
+++ dumbbellTopoNet.py
@@ -2,6 +2,7 @@
 
 from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 
@@ -21,23 +22,43 @@ class dumbbellTopo(Topo):
         sourceHost2 = self.addHost('sH2')
         receiverHost1 = self.addHost('rH1')
         receiverHost2 = self.addHost('rH2')
-        
+       
+        #link speeds (in p/ms)
+        backBoneSpeedpms = 82
+        accessSpeedpms = 21
+        srcSpeedpms = 80
+        rcvSpeedpms = 80
+
+        #link speeds (in Mbps)
+        msToSec = 1000
+        bitsPerPacket = 12000 #a regular 1500byte packet has 1500*8 = 12000 bits
+        conversionFactor = msToSec * bitsPerPacket
+        backBoneSpeed = backBoneSpeedpms * conversionFactor #984Mbps
+        accessSpeed = accessSpeedpms * conversionFactor #252Mbps
+        srcSpeed = srcSpeedpms * conversionFactor #960Mbps
+        rcvSpeed = rcvSpeedpms * conversionFactor #960Mbps
+
+
+        #propagation delays (in ms)
+        shortDelay = 21
+        mediumDelay = 81
+        longDelay = 162
+
         #links
         self.addLink(backBoneRouter1, backBoneRouter2)
         self.addLink(accessRouter1, backBoneRouter1)
         self.addLink(accessRouter2, backBoneRouter2)
-        
         self.addLink(sourceHost1, accessRouter1)
         self.addLink(sourceHost2, accessRouter1)
         self.addLink(receiverHost1, accessRouter2)
         self.addLink(receiverHost2, accessRouter2)
 
 
+
 def simpleTest():
     "Create and test a simple network"
-    #topo = SingleSwitchTopo(n=4)
     topo = dumbbellTopo()
-    net = Mininet(topo)
+    net = Mininet(topo=topo, link=TCLink)
     net.start()
     print( "Dumping host connections" )
     dumpNodeConnections(net.hosts)
